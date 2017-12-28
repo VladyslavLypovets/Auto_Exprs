6f58d28b24bf3450b506f6a43dc513a244af6ad5
Vladyslav Lypovets <avasl333@gmail.com>
2017-12-28 18:39:58 +0200

    hoftfix and add else_services.html, header_login.html

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,9 @@
 $(function() {
   $(document).ready(function(){
     $('select').selectpicker();
-      VK.Widgets.Group("vk_groups", {mode: 3}, 20003922);
-  });
     $('.datepicker').datepicker({
         language: "ru",
         autoclose: true
     });
+  });
 });
-
-
