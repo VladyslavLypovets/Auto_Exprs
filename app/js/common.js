76e96873183c952f7a6190ad7021131cd8c6d143
Vladyslav Lypovets <avasl333@gmail.com>
2018-01-09 18:05:44 +0200

    finish decstope

--- app/js/common.js
+++ app/js/common.js
@@ -5,14 +5,26 @@ $(function() {
         language: "ru",
         autoclose: true
     });
-  });
-  $('label[for="male"] button').click(function(){
-    $('#female').attr("checked", false);
-    $('#male').attr("checked", true);
+    $('.hover-column').hover(function(){
+      var img = $(this).attr('data-image');
+      var pos = parseInt($(this).attr('data-pos'))-1;
 
-  });
-  $('label[for="female"] button').click(function(){
-    $('#male').attr("checked", false);
-    $('#female').attr("checked", true);
+      $container = $(this).parent();
+
+      $container.prev().find('.controll-image').removeClass('active');
+      $($container.prev().find('.controll-image').get(pos)).addClass('active');
+
+      $container.next().find('img').removeClass('active');
+      $($container.next().find('img').get(pos)).addClass('active');
+    });
+    $('label[for="male"] button').click(function(){
+      $('#female').attr("checked", false);
+      $('#male').attr("checked", true);
+
+    });
+    $('label[for="female"] button').click(function(){
+      $('#male').attr("checked", false);
+      $('#female').attr("checked", true);
+    });
   });
 });
